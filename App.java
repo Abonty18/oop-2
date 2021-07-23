13232be648bab77f1c353480b2c1707141e1aef2
Abonty18 <labibafarah2998@gmail.com>
2021-07-23 12:33:16 +0600

    Add files via upload

--- App.java
+++ App.java
@@ -12,10 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBase;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -36,7 +33,8 @@ public class App extends Application {
     //GUI
     private GridPane gridPane=new GridPane();
     private BorderPane borderPane=new BorderPane();
-    private Label title=new Label("Theme");
+
+    //private Label title=new Label("Theme");
     //private Button Random_AI_Button=new Button("Start With Random AI");
 
 
@@ -44,7 +42,8 @@ public class App extends Application {
 
     //public void getTheme(ActionEvent event);
 
-    Font font=Font.font("Arial",FontWeight.BOLD,25);
+    Font font=Font.font("Arial",FontWeight.BOLD,21);
+    Font font2=Font.font("Arial",FontWeight.BOLD,17);
 
     private Button[]btns=new Button[9];
 
@@ -55,6 +54,8 @@ public class App extends Application {
         this.createGUI();
         //this.handleEvent();
 
+        //HBox hBox1=new HBox();
+        //HBox hBox2=new HBox();
 
         Scene scene=new Scene(borderPane,850,650);
         stage.setScene(scene);
@@ -63,10 +64,24 @@ public class App extends Application {
     }
     //creating GUI
     private void createGUI() {
+
+
+        HBox hBox1=new HBox();
+        HBox hBox2=new HBox();
         VBox right =new VBox();
+        VBox up=new VBox();
+        Label title=new Label("Theme");
+        //VBox.setMargin(up,new Insets(250,250,250,250));
+        //VBox.setMargin(right,new Insets(250,250,250,250));
+        up.setPrefWidth(250);
+        up.setPrefHeight(35);
+        //up.setPadding(new Insets(200,30,50,350));
+        up.setSpacing(10);
+        title.setMinWidth(up.getPrefWidth());
+        title.setMinHeight(up.getPrefHeight());
         right.setPrefWidth(250);
         right.setPrefHeight(35);
-        right.setPadding(new Insets(10,50,50,50));
+        //right.setPadding(new Insets(10,50,50,50));
         right.setSpacing(10);
         Button button1=new Button("Start With Random AI");
         Button button2=new Button("Start With Defensive AI");
@@ -75,11 +90,23 @@ public class App extends Application {
         button2.setMinWidth(right.getPrefWidth());
         button2.setMinHeight(right.getPrefHeight());
         right.getChildren().addAll(button1,button2);
+        up.getChildren().addAll(title);
+        hBox2.getChildren().addAll(up,right);
+        hBox1.getChildren().addAll(gridPane);
+        hBox1.setPadding(new Insets(20,20,20,20));
+        hBox2.setPadding(new Insets(20,20,20,20));
+        //hBox1.setAlignment(Pos.CENTER_LEFT);
+        //hBox2.setAlignment(Pos.CENTER_RIGHT);
+
+
+
         //title.setLayoutX(700);
 
 
         //Creating title;
         title.setFont(font);
+        button1.setFont(font2);
+        button2.setFont(font2);
         //Random_AI_Button.setFont(font);
         //Defensive_AI_Button.setFont(font);
         title.setAlignment(Pos.TOP_RIGHT);
@@ -88,7 +115,7 @@ public class App extends Application {
 
         //creating restart button
         //themeButton.setFont(font);
-        borderPane.setTop(title);
+        borderPane.setTop(up);
         //borderPane.setCenter(Random_AI_Button);
         //Random_AI_Button.setMaxSize(200,20);
         //borderPane.setBottom(Defensive_AI_Button);
@@ -96,12 +123,12 @@ public class App extends Application {
 
         //borderPane.setPadding(Random_AI_Button);
         borderPane.setBottom(right);
-       // borderPane.setRight(right);
+        //borderPane.setRight(right);
         //borderPane.setBottom(Defensive_AI_Button);
-        BorderPane.setAlignment(title, Pos.TOP_RIGHT);
+        BorderPane.setAlignment(up, Pos.TOP_RIGHT);
         BorderPane.setAlignment(right,Pos.BOTTOM_RIGHT);
         //BorderPane.setAlignment(Defensive_AI_Button,Pos.BASELINE_RIGHT);
-        borderPane.setPadding(new Insets(15,15,15,15));
+        //borderPane.setPadding(new Insets(10,10,10,10));
         //9 buttons
         int label=0;
         for(int i=0;i<3;i++)
@@ -115,6 +142,7 @@ public class App extends Application {
                 button.setPrefHeight(150);
                 gridPane.add(button,j,i);
                 gridPane.setAlignment(Pos.CENTER_LEFT);
+                //gridPane.setPadding(new Insets(10,10,10,10));
                 btns[label]=button;
                 label++;
 
